855bd0396f3eb570cfde6e648a2cfd6d5ea953b2
rockyxd3 <266638555+rockyxd3@users.noreply.github.com>
2026-03-18 11:47:46 +0000

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ + #⟶̽ जय श्री ༢།म >𝟑🙏🚩

--- AloneMusic/platforms/Youtube.py
+++ AloneMusic/platforms/Youtube.py
@@ -3,35 +3,49 @@
 import asyncio
 import os
 import re
-from typing import Union
-import yt_dlp
-from pyrogram.enums import MessageEntityType
-from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
-from AloneMusic.utils.formatters import time_to_seconds
+
 import aiohttp
 from Oneforall import LOGGER
+from youtubesearchpython.__future__ import VideosSearch
+
+from AloneMusic.utils.formatters import time_to_seconds
 
 logger = LOGGER("AloneMusic.platforms.Youtube")
 
 # ---------------- SECURITY CONSTANTS ---------------- #
 
-YOUTUBE_URL_PATTERN = re.compile(
-    r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/"
-)
+YOUTUBE_URL_PATTERN = re.compile(r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/")
 
 DANGEROUS_KEYWORDS = [
     # adult
-    "porn","xxx","sex","hentai","nude","naked","onlyfans","adult",
-
+    "porn",
+    "xxx",
+    "sex",
+    "hentai",
+    "nude",
+    "naked",
+    "onlyfans",
+    "adult",
     # drugs
-    "weed","marijuana","cocaine","heroin","meth","drug","lsd",
-
+    "weed",
+    "marijuana",
+    "cocaine",
+    "heroin",
+    "meth",
+    "drug",
+    "lsd",
     # violent / dangerous
-    "beheading","bomb","explosive","cartel","terrorist",
-
+    "beheading",
+    "bomb",
+    "explosive",
+    "cartel",
+    "terrorist",
     # piracy / copyright bait
-    "full movie","free movie","leaked","camrip","pirated"
+    "full movie",
+    "free movie",
+    "leaked",
+    "camrip",
+    "pirated",
 ]
 
 YOUR_API_URL = None
@@ -39,6 +53,7 @@ FALLBACK_API_URL = "https://vercel.com/txkuzes-projects/admin-music-hub"
 
 
 # ---------------- FILTER FUNCTIONS ---------------- #
+
 
 def contains_dangerous(text: str) -> bool:
     if not text:
@@ -57,6 +72,7 @@ def validate_youtube_url(link: str) -> bool:
 
 
 # ---------------- LOAD API ---------------- #
+
 
 async def load_api_url():
     global YOUR_API_URL
@@ -93,6 +109,7 @@ except RuntimeError:
 
 
 # ---------------- DOWNLOAD AUDIO ---------------- #
+
 
 async def download_song(link: str) -> str:
 
@@ -160,6 +177,7 @@ async def download_song(link: str) -> str:
 
 # ---------------- SAFE PLAYLIST FETCH ---------------- #
 
+
 async def get_playlist_ids(link: str, limit: int):
 
     process = await asyncio.create_subprocess_exec(
@@ -184,6 +202,7 @@ async def get_playlist_ids(link: str, limit: int):
 
 
 # ---------------- MAIN CLASS ---------------- #
+
 
 class YouTubeAPI:
 
@@ -221,7 +240,6 @@ class YouTubeAPI:
 
             return title, duration, duration_sec, thumbnail, vidid
 
-
     async def playlist(self, link, limit, user_id):
 
         if "&" in link:
@@ -233,7 +251,6 @@ class YouTubeAPI:
         ids = await get_playlist_ids(link, limit)
 
         return ids
-
 
     async def track(self, link: str):
 
